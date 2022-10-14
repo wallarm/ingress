92b9d8f0694735153cb22ec08ad7e64ac6fc8a56
Evgeniy Dmitriev <edmitriev@wallarm.com>
2022-10-14 14:24:23 +0700

    [devenv] Added ability to load helper images from private registry

--- build/load-images.sh
+++ build/load-images.sh
@@ -9,6 +9,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 KIND_WORKERS="${KIND_WORKERS:-${KIND_CLUSTER_NAME}-control-plane}"
 
 HELPER_TAG="${HELPER_TAG:-$(cat "${DIR}"/../TAG)}"
+HELPER_REGISTRY="${HELPER_REGISTRY:-docker.io/wallarm}"
 
 HELPER_IMAGES=(
   ingress-ruby
@@ -19,9 +20,9 @@ HELPER_IMAGES=(
 
 echo "Kind cluster name: ${KIND_CLUSTER_NAME}"
 echo "Kind workers: ${KIND_WORKERS}"
-for ITEM in "${HELPER_IMAGES[@]}"; do
-  IMAGE="${REGISTRY}/${ITEM}"
-  docker pull --quiet "${IMAGE}:${HELPER_TAG}"
-  docker tag "${IMAGE}:${HELPER_TAG}" "${IMAGE}:${TAG}"
-  kind load docker-image --name="${KIND_CLUSTER_NAME}" --nodes="${KIND_WORKERS}" "${IMAGE}:${TAG}"
+echo "Helper images registry: ${HELPER_REGISTRY}"
+for IMAGE in "${HELPER_IMAGES[@]}"; do
+  docker pull "${HELPER_REGISTRY}/${IMAGE}:${HELPER_TAG}"
+  docker tag "${HELPER_REGISTRY}/${IMAGE}:${HELPER_TAG}" "${REGISTRY}/${IMAGE}:${TAG}"
+  kind load docker-image --name="${KIND_CLUSTER_NAME}" --nodes="${KIND_WORKERS}" "${REGISTRY}/${IMAGE}:${TAG}"
 done
