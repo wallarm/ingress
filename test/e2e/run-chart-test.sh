41e361f90e2ae1de981f69125a37a340c5f2cce0
Evgeniy Dmitriev <edmitriev@wallarm.com>
2023-01-11 01:18:25 +0700

    DEVOPS-1835: Added helm-upgrade feature in CI workflow

--- test/e2e/run-chart-test.sh
+++ test/e2e/run-chart-test.sh
@@ -30,8 +30,10 @@ cleanup() {
     kind "export" logs --name ${KIND_CLUSTER_NAME} "${ARTIFACTS}/logs" || true
   fi
   if [[ "${CI:-}" == "true" ]]; then
-    kind delete cluster \
-      --name ${KIND_CLUSTER_NAME}
+    kind delete cluster --name "${KIND_CLUSTER_NAME}"
+  else
+    rm -f ct.sh
+    rm -rf ct_previous_revision*
   fi
 }
 
@@ -44,17 +46,20 @@ if ! command -v kind --version &> /dev/null; then
   exit 1
 fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 # Use 1.0.0-dev to make sure we use the latest configuration in the helm template
 export TAG=1.0.0-dev
 export ARCH=${ARCH:-amd64}
 
-BASEDIR=$(dirname "$0")
+# The image for chart testing tool
+CT_IMAGE="quay.io/helmpack/chart-testing:v3.7.1-amd64"
+CT_NAMESPACE="ct"
 
-# Uses a custom chart-testing image to avoid timeouts waiting for namespace deletion.
-# The changes can be found here: https://github.com/xDmitriev/chart-testing/commit/aa221da0c1fd09c0190e604493f12a4b5e155c13
-CT_IMAGE="quay.io/dmitriev/chart-testing@sha256:c0bd16c255b1c10697675f5a4f77d8844c3f5a598ab64e0d17286aa1a01a019c"
+# Handle the case when we run chart testing with '--upgrade' option
+if [[ "${CT_MODE:-}" == "upgrade" ]]; then
+  CT_ARGS="--target-branch main --remote origin --upgrade --skip-missing-values"
+else
+  CT_ARGS="--charts charts/ingress-nginx"
+fi
 
 HELM_EXTRA_SET_ARGS="\
  --set controller.wallarm.apiHost=${WALLARM_API_HOST:-api.wallarm.com} \
@@ -64,6 +69,9 @@ HELM_EXTRA_SET_ARGS="\
  --set controller.image.tag=1.0.0-dev \
  --set controller.terminationGracePeriodSeconds=0 \
  --set fullnameOverride=wallarm-ingress ${HELM_EXTRA_SET_ARGS:-}"
+
+SECRET_NAME="wallarm-api-token"
+SECRET_KEY="token"
 
 export KUBECONFIG="${KUBECONFIG:-$HOME/.kube/kind-config-$KIND_CLUSTER_NAME}"
 
@@ -96,23 +104,42 @@ fi
 export KIND_WORKERS=$(kind get nodes --name="${KIND_CLUSTER_NAME}" | grep 'worker' | awk '{printf (NR>1?",":"") $1}')
 
 echo "[dev-env] copying docker images to cluster..."
-kind load docker-image --name="${KIND_CLUSTER_NAME}" --nodes=${KIND_WORKERS} wallarm/ingress-controller:${TAG}
+kind load docker-image --name="${KIND_CLUSTER_NAME}" --nodes="${KIND_WORKERS}" wallarm/ingress-controller:${TAG}
 
 echo "[dev-env] copying helper images to cluster..."
-${DIR}/../../build/load-images.sh
-set -x
+"${CURDIR}"/build/load-images.sh
+
+if ! kubectl get namespace ${CT_NAMESPACE} &> /dev/null; then
+  echo "[dev-env] creating namespace for chart testing..."
+  kubectl create namespace ${CT_NAMESPACE}
+fi
+
+if ! kubectl -n ${CT_NAMESPACE} get secret "${SECRET_NAME}" &> /dev/null; then
+  echo "[dev-env] creating secret ${SECRET_NAME}..."
+  kubectl -n ${CT_NAMESPACE} create secret generic "${SECRET_NAME}" --from-literal="${SECRET_KEY}"="${WALLARM_API_TOKEN}"
+fi
+
+cat <<EOF > ct.sh
+#!/bin/bash
+set -e
+git rev-parse --is-inside-work-tree &> /dev/null || git config --global --add safe.directory /workdir
+exec ct install \
+ --namespace ${CT_NAMESPACE} \
+ --helm-extra-set-args "${HELM_EXTRA_SET_ARGS}" \
+ --helm-extra-args "--timeout 180s" \
+ ${CT_ARGS:-} \
+ --debug
+EOF
+chmod +x ct.sh
+
 echo "[dev-env] running helm chart e2e tests..."
 docker run \
     --rm \
     --interactive \
     --network host \
     --name ct \
-    --volume "${KUBECONFIG}":/root/.kube/config \
-    --volume "${DIR}/../../":/workdir \
+    --volume "${KUBECONFIG}:/root/.kube/config" \
+    --volume "${CURDIR}:/workdir" \
     --workdir /workdir \
-    ${CT_IMAGE} ct install \
-        --charts charts/ingress-nginx \
-        --helm-extra-set-args "${HELM_EXTRA_SET_ARGS}" \
-        --helm-extra-args "--timeout 90s" \
-        ${CT_EXTRA_ARGS:-} \
-        --debug
+    --entrypoint /workdir/ct.sh \
+    ${CT_IMAGE}
