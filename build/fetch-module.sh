33e38798064e813e09cb04b7f9cb5a71c53d7a4b
Nikita Nedvetskii <nnedvetskii@wallarm.com>
2023-10-17 11:33:27 +0200

    DEVOPS-2249 Re-add blocking page to image

--- build/fetch-module.sh
+++ build/fetch-module.sh
@@ -3,6 +3,7 @@
 set -e
 
 TARGETS_MODULES="rootfs/modules"
+TARGETS_PAGES="rootfs/usr"
 
 AIO_VERSION=$(cat AIO_BASE)
 [ "${ARCH}" == "amd64" ] && AIO_ARCH=x86_64 || AIO_ARCH=aarch64
@@ -14,7 +15,7 @@ NGINX_VER=1216
 
 # grep on Mac OS comes from FreeBSD not GNU, and it does not have option -P. Need to install ggrep by `brew install grep`
 GREP_CMD=$(which grep)
-STRIP_N=4
+STRIP_N=0
 if [[ "$OSTYPE" == darwin* ]]; then
   if ! command -v ggrep &> /dev/null
   then
@@ -22,7 +23,7 @@ if [[ "$OSTYPE" == darwin* ]]; then
     exit 1
   fi
   GREP_CMD=$(which ggrep)
-  STRIP_N=5
+  STRIP_N=1
 fi
 
 
@@ -30,10 +31,14 @@ if ! test -f "${AIO_FILE}"; then
   echo "Downloading AIO archive (${ARCH}/${AIO_ARCH})"
   curl -L -C - -o "${AIO_FILE}" "${AIO_URL}"
 fi
+
 mkdir -p "${TARGETS_MODULES}/${ARCH}"
 echo "Extracting ngx_http_wallarm_module (${ARCH}/${AIO_ARCH})"
-tar -xvf "${AIO_FILE}" -C "${TARGETS_MODULES}/${ARCH}" --strip-components="${STRIP_N}" "/opt/wallarm/modules/ingress-${NGINX_VER}/ngx_http_wallarm_module.so"
+tar -xvf "${AIO_FILE}" -C "${TARGETS_MODULES}/${ARCH}" --strip-components="$((4 + ${STRIP_N}))" "/opt/wallarm/modules/ingress-${NGINX_VER}/ngx_http_wallarm_module.so"
 
+mkdir -p "${TARGETS_PAGES}"
+echo "Extracting wallarm_blocked.html page"
+tar -xvf "${AIO_FILE}" -C "${TARGETS_PAGES}" --strip-components="$((6 + ${STRIP_N}))" "/opt/wallarm/usr/share/nginx/html/wallarm_blocked.html"
 
 # image rm is needed to be able to pull two samely tagged images for different platforms (as we execute on the same runner)
 NGINX_BASE=$(cat NGINX_BASE)
