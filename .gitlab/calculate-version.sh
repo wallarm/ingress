1de64d36cbab80ea6bada95fb379f44c77f1bba0
Nikita Nedvetskii <nnedvetskii@wallarm.com>
2025-05-09 18:45:48 +0200

    NODE-6226 Fix version calculation

--- .gitlab/calculate-version.sh
+++ .gitlab/calculate-version.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 if [[ ! $AIO_VERSION =~ "rc" ]]; then
-    if docker pull -q wallarm/ingress-controller:${AIO_VERSION} ; then
-        echo "Image AIO_VERSION $AIO_VERSION exists already, will increment IC patch version..."
-        IFS=- read -r VERSION SUFFIX <<< "$AIO_VERSION"
-        IFS=. read -r MAJOR MINOR PATCH <<< "$VERSION"
+    IFS=- read -r VERSION SUFFIX <<< "$AIO_VERSION"
+    IFS=. read -r MAJOR MINOR PATCH <<< "$VERSION"
 
-        helm repo add wallarm https://charts.wallarm.com && helm repo update wallarm
-        LATEST=$(helm search repo wallarm/wallarm-ingress --version ^${MAJOR}.${MINOR} -o json | jq -r '.[].version')
+    helm repo add wallarm https://charts.wallarm.com && helm repo update wallarm
+    LATEST=$(helm search repo wallarm/wallarm-ingress --version ^${MAJOR}.${MINOR} -o json | jq -r '.[].version')
+
+    if [ -z "$LATEST" ]; then
+        LATEST_PATCH=-1
+    else
         LATEST_PATCH=$(cut -d'.' -f3 <<< $LATEST)
-        echo "Detected latest release as $LATEST"
+    fi
+    echo "Detected latest release as ${LATEST:-none}"
 
+    if [ $PATCH -gt $LATEST_PATCH ]; then
+        echo "Chart with version $AIO_VERSION doesn't exist yet, re-using AIO_VERSION for the new chart version"
+        TAG=$AIO_VERSION
+    else
+        echo "Chart with version $AIO_VERSION (or later) exists already, will increment chart patch version..."
         TAG=$(echo "${MAJOR}.${MINOR}.$((${LATEST_PATCH} + 1))")
         [ ! -z $SUFFIX ] && TAG="${TAG}-${SUFFIX}"
-    else
-        echo "Image with tag $AIO_VERSION doesn't exist yet, re-using AIO_VERSION for the new image tag"
-        TAG=$AIO_VERSION
     fi
 else
     echo "AIO_VERSION is RC, if IC image with the same tag exists already it will be overwritten"
     TAG=$AIO_VERSION
 fi
 
+# Generally, we assume that versions are in sync.
+# But cases when ingress version (chart version) is larger than that of AIO, and/ or of controller image, may occur, due to ingress-specific patching
+# In these cases, we use the chart version as a reference for "latest", and publish new controller image and chart using that version and adding +1
 echo "TAG=$TAG" > version.env
